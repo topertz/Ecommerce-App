4d3b7843f7fcd1b7bd3961c0938aff2df18a5595
topertz <k.markuci@gmail.com>
2026-09-02 09:58:25 +0200

    Improve admin product buttons styling

--- backend/fix-admin.js
+++
@@ -1,9 +0,0 @@
-const db = require('./database');
-
-const result = db
-	.prepare("UPDATE users SET role = ? WHERE username = ?")
-	.run('admin', 'admin123');
-
-console.log('Updated rows:', result.changes);
-
-db.close();
